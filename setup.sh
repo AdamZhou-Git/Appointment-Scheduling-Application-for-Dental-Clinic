e80e4563c535ba9434abc5fe08199269ff32e562
dunhuizhou <dunhuizhou@gmail.com>
2022-04-08 22:49:12 -0600

    2nd upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://xuhbuxzjmscdhj:b8862dba5acc377c6baaabd42823ba638b4655e3304cf3a1b5957bc17af413ff@ec2-3-230-122-20.compute-1.amazonaws.com:5432/d8k5a1a4banmrj"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
